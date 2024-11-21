5fd7870bc5ade43c2cc1f69cee8566497cff0ccf
Ryan Jacob <jacobry@mail.uc.edu>
2024-11-21 11:31:26 -0500

    Added SayGoodbye()

--- inClass20241121-IS3050-001/HelloWorld.cs
+++ inClass20241121-IS3050-001/HelloWorld.cs
@@ -7,9 +7,13 @@ namespace inClass20241121_IS3050_001
 {
     public class HelloWorld
     {
+        public static void SayGoodbye()
+        {
+            Console.WriteLine("Goodbye world from SayGoodbye()");
+        }
         public static void SayHello() 
-        { 
-            Console.WriteLine("Hello from HelloWorld.SayHello()")
+        {
+            Console.WriteLine("Hello from HelloWorld.SayHello()");
         }
     }
 }
